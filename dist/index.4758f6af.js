4e1e02fe6cf8b44f54d6e12092b30bf23b824437
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-02-23 10:56:43 +0100

    cambios

--- dist/index.4758f6af.js
+++ dist/index.4758f6af.js
@@ -1,35 +1,20 @@
-const carouselSlide = document.querySelector(".section-escenarios__contenedor-imagenes");
-const carouselImages = document.querySelectorAll(".contenedor-imagenes__escenario");
+const carouselContainer = document.querySelector(".section-escenarios__contenedor-imagenes");
 const carouselDots = document.querySelectorAll(".puntos__punto");
-// Inicializar valores
-let counter = 1;
-const slideWidth = carouselImages[0].clientWidth;
-// Configurar el desplazamiento inicial
-carouselSlide.style.transform = "translateX(" + -slideWidth * counter + "px)";
-// Añadir los listeners de los puntos
+let currentIndex = 0;
 carouselDots.forEach((dot, index)=>{
     dot.addEventListener("click", ()=>{
-        counter = index + 1;
-        carouselSlide.style.transition = "transform 0.4s ease-in-out";
-        carouselSlide.style.transform = "translateX(" + -slideWidth * counter + "px)";
-        // Actualizar los puntos activos
-        carouselDots.forEach((dot)=>{
-            dot.classList.remove("activo");
-        });
-        dot.classList.add("activo");
+        currentIndex = index;
+        updateCarousel();
     });
 });
-// Añadir el listener para el desplazamiento automático
+function updateCarousel() {
+    carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+    carouselDots.forEach((dot)=>dot.classList.remove("activo"));
+    carouselDots[currentIndex].classList.add("activo");
+}
 setInterval(()=>{
-    counter++;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    carouselSlide.style.transform = "translateX(" + -slideWidth * counter + "px)";
-    // Actualizar los puntos activos
-    if (counter > carouselImages.length - 1) counter = 0;
-    carouselDots.forEach((dot)=>{
-        dot.classList.remove("activo");
-    });
-    carouselDots[counter].classList.add("activo");
-}, 3000);
+    currentIndex = (currentIndex + 1) % carouselDots.length;
+    updateCarousel();
+}, 5000);
 
 //# sourceMappingURL=index.4758f6af.js.map
--- src/assets/js/carrusel.js
+++ src/assets/js/carrusel.js
@@ -1,39 +1,15 @@
-const carouselSlide = document.querySelector('.section-escenarios__contenedor-imagenes');
-const carouselImages = document.querySelectorAll('.contenedor-imagenes__escenario');
-const carouselDots = document.querySelectorAll('.puntos__punto');
+const nextBtn = document.querySelector("#nextBtn");
+const carouselImages = document.querySelector(".carousel-images");
 
-// Inicializar valores
-let counter = 1;
-const slideWidth = carouselImages[0].clientWidth;
+let scrollLeft = 0;
 
-// Configurar el desplazamiento inicial
-carouselSlide.style.transform = 'translateX(' + (-slideWidth * counter) + 'px)';
-
-// Añadir los listeners de los puntos
-carouselDots.forEach((dot, index) => {
-  dot.addEventListener('click', () => {
-    counter = index + 1;
-    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-    carouselSlide.style.transform = 'translateX(' + (-slideWidth * counter) + 'px)';
-    // Actualizar los puntos activos
-    carouselDots.forEach(dot => {
-      dot.classList.remove('activo');
-    });
-    dot.classList.add('activo');
+nextBtn.addEventListener("click", () => {
+  scrollLeft += carouselImages.offsetWidth;
+  if (scrollLeft > carouselImages.scrollWidth) {
+    scrollLeft = 0;
+  }
+  carouselImages.scrollTo({
+    left: scrollLeft,
+    behavior: "smooth",
   });
 });
-
-// Añadir el listener para el desplazamiento automático
-setInterval(() => {
-  counter++;
-  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-  carouselSlide.style.transform = 'translateX(' + (-slideWidth * counter) + 'px)';
-  // Actualizar los puntos activos
-  if (counter > carouselImages.length - 1) {
-    counter = 0;
-  }
-  carouselDots.forEach(dot => {
-    dot.classList.remove('activo');
-  });
-  carouselDots[counter].classList.add('activo');
-}, 3000);
