81737d459aec294271fe05ef603419fe93f844a4
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-02-23 12:23:14 +0100

    volviendo al carrusel anterior

--- dist/index.4758f6af.js
+++ dist/index.4758f6af.js
@@ -1,20 +1,28 @@
-const carouselContainer = document.querySelector(".section-escenarios__contenedor-imagenes");
-const carouselDots = document.querySelectorAll(".puntos__punto");
-let currentIndex = 0;
-carouselDots.forEach((dot, index)=>{
-    dot.addEventListener("click", ()=>{
-        currentIndex = index;
-        updateCarousel();
+const grande = document.querySelector(".carrusel-escenarios__contenedor");
+const punto = document.querySelector(".puntos__punto");
+// Cuando se hace click en punto
+// Saber la posición de ese punto
+// Aplicar un transform translateX al grande
+// Quitar la clase activo de todos los puntos
+// Añadir la clase activo al punto que hemos hecho click
+// Recorrer todos los punto
+punto.forEach((cadaPunto, i)=>{
+    // Asignamos un click a cadaPunto
+    punto[i].addEventListener("click", ()=>{
+        // Guardar la posición de ese punto
+        let posicion = i;
+        // Calculando el espacio que debe desplazarse el grande
+        let operacion = posicion * -50;
+        // Movemos el grande
+        grande.style.transform = `translateX(${operacion}%)`;
+        // Recorremos todos los punto
+        punto.forEach((cadaPunto, i)=>{
+            // Quitamos la clase activo a todos los punto
+            punto[i].classList.remove("activo");
+        });
+        //Añadir la clase activo en el punto que hemos hecho click
+        punto[i].classList.add("activo");
     });
 });
-function updateCarousel() {
-    carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
-    carouselDots.forEach((dot)=>dot.classList.remove("activo"));
-    carouselDots[currentIndex].classList.add("activo");
-}
-setInterval(()=>{
-    currentIndex = (currentIndex + 1) % carouselDots.length;
-    updateCarousel();
-}, 5000);
 
 //# sourceMappingURL=index.4758f6af.js.map
--- src/assets/js/carrusel.js
+++ src/assets/js/carrusel.js
@@ -1,15 +1,34 @@
-const nextBtn = document.querySelector("#nextBtn");
-const carouselImages = document.querySelector(".carousel-images");
+const grande = document.querySelector('.carrusel-escenarios__contenedor')
+const punto = document.querySelector('.puntos__punto')
 
-let scrollLeft = 0;
+// Cuando se hace click en punto
+    // Saber la posición de ese punto
+    // Aplicar un transform translateX al grande
+    // Quitar la clase activo de todos los puntos
+    // Añadir la clase activo al punto que hemos hecho click
 
-nextBtn.addEventListener("click", () => {
-  scrollLeft += carouselImages.offsetWidth;
-  if (scrollLeft > carouselImages.scrollWidth) {
-    scrollLeft = 0;
-  }
-  carouselImages.scrollTo({
-    left: scrollLeft,
-    behavior: "smooth",
-  });
-});
+
+
+// Recorrer todos los punto
+punto.forEach( ( cadaPunto, i)=> {
+    // Asignamos un click a cadaPunto
+    punto[i].addEventListener('click',()=> {
+
+        // Guardar la posición de ese punto
+        let posicion = i
+        // Calculando el espacio que debe desplazarse el grande
+        let operacion = posicion * -50
+
+        // Movemos el grande
+        grande.style.transform = `translateX(${ operacion }%)`
+
+        // Recorremos todos los punto
+        punto.forEach( ( cadaPunto, i)=> {
+            // Quitamos la clase activo a todos los punto
+            punto[i].classList.remove('activo')
+        })
+        //Añadir la clase activo en el punto que hemos hecho click
+        punto[i].classList.add('activo')
+
+    })
+})
